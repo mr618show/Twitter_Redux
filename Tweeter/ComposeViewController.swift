9ec33ca7f6f3301e53387fdaf461e2cc36e3a4d7
Rui Mao <ruimao18@gmail.com>
2017-04-20 16:43:41 -0700

    countdown label update app name

--- Tweeter/ComposeViewController.swift
+++ Tweeter/ComposeViewController.swift
@@ -8,27 +8,44 @@
 
 import UIKit
 
-class ComposeViewController: UIViewController {
+class ComposeViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var thumImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var tweetContent: UITextField!
-    
+    @IBOutlet weak var screenNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UITextView!
     @IBOutlet weak var countDownLabel: UILabel!
+    
     var isReply: Bool = false
     var tweetID: Int?
     var replyTo: String?
     override func viewDidLoad() {
     
         super.viewDidLoad()
+        self.countDownLabel.text = "140"
+        self.tweetContent.delegate = self
+        self.updateCharacterCount()
+        
         
         //Setting Nav bar color
         let twitterColor = UIColor(red: 29/256, green: 202/256, blue: 255/256, alpha: 1.0)
-        navigationController?.navigationBar.barTintColor = twitterColor
-        navigationController?.navigationBar.tintColor = UIColor.white
-        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        if let navigationBar = self.navigationController?.navigationBar {
+        navigationBar.barTintColor = twitterColor
+        navigationBar.tintColor = UIColor.white
+        navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        //add countDown lable in nav bar
+            /*let countDownFrame = CGRect(x: (navigationBar.frame.width) * 5/8, y: 0, width: navigationBar.frame.width/2, height: navigationBar.frame.height)
+            
+            let countDownLabel = UILabel(frame: countDownFrame)
+            countDownLabel.text = "140"
+            navigationBar.addSubview(countDownLabel)
+        */
+        }
+        
         
         self.nameLabel.text = User.currentUser?.name as String?
+        self.screenNameLabel.text = String("@")! + (User.currentUser?.screenname as String?)!
         thumImageView.setImageWith(User.currentUser!.profileUrl as! URL)
+        
 
         // Do any additional setup after loading the view.
     }
@@ -66,8 +83,31 @@ class ComposeViewController: UIViewController {
         }
     }
     
+    func updateCharacterCount() {
+        
+        self.countDownLabel.reloadInputViews()
+        self.countDownLabel.text = "\(140 - (self.tweetContent.text?.characters.count)!)"
+    }
     
-}
+    func textViewDidChange(_ textView: UITextView) {
+        if 140 - (tweetContent.text?.characters.count)! < 20 {
+            countDownLabel.textColor = UIColor.red
+            if 140 - (tweetContent.text?.characters.count)! < 0 {
+                self.navigationItem.rightBarButtonItem?.isEnabled = false
+            }
+        } else {
+            countDownLabel.textColor = UIColor.gray
+        }
+        self.updateCharacterCount()
+    }
+    
+
+    
+  
+    }
+    
+    
+
 
 
 
